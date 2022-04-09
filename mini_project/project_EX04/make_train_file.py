cbe2a56cff8a668f626d9789b4582739aeeff20b
Codetesting <37256601+Codetesing@users.noreply.github.com>
2022-04-09 23:11:55 +0900

    Project_EX04
    Project_EX04

---
+++ mini_project/project_EX04/make_train_file.py
@@ -0,0 +1,14 @@
+import os
+
+image_files = []
+os.chdir(os.path.join("example_photo/images"))
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".jpg"):
+        image_files.append(filename)
+os.chdir("..")
+with open("train.txt", "w") as outfile:
+    for image in image_files:
+        outfile.write("data/fruits/example_photo/images/" + image)
+        outfile.write("\n")
+    outfile.close()
+os.chdir("..")
