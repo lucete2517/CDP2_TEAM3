73444a0f14473cd5ec6b3027cb4669819036e76a
winter930 <winter930@naver.com>
2022-05-17 00:51:57 +0900

    이미지변환
    이미지변환(가우스, 흑백, 스레스홀드)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace TestApp
+namespace imageblack
 {
     partial class Form1
     {
@@ -29,242 +29,114 @@ namespace TestApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.picPreview = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CB = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.picSnapShot = new System.Windows.Forms.PictureBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.btnPlay = new System.Windows.Forms.Button();
-            this.btnSnapShot = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picSnapShot)).BeginInit();
             this.SuspendLayout();
             // 
-            // picPreview
+            // pictureBox1
             // 
-            this.picPreview.Image = ((System.Drawing.Image)(resources.GetObject("picPreview.Image")));
-            this.picPreview.Location = new System.Drawing.Point(17, 47);
-            this.picPreview.Name = "picPreview";
-            this.picPreview.Size = new System.Drawing.Size(312, 270);
-            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picPreview.TabIndex = 1;
-            this.picPreview.TabStop = false;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 330);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // panel1
+            // CB
             // 
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(675, 19);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(255, 298);
-            this.panel1.TabIndex = 2;
+            this.CB.Location = new System.Drawing.Point(404, 21);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(113, 41);
+            this.CB.TabIndex = 1;
+            this.CB.Text = "convert black";
+            this.CB.UseVisualStyleBackColor = true;
+            this.CB.Click += new System.EventHandler(this.CB_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.search_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(15, 195);
+            this.pictureBox2.Location = new System.Drawing.Point(404, 82);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(126, 96);
+            this.pictureBox2.Size = new System.Drawing.Size(330, 330);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
-            // label5
+            // button2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 164);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(37, 15);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "나이";
+            this.button2.Location = new System.Drawing.Point(556, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "gaussian";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.gaussian_Click);
             // 
-            // textBox3
+            // button3
             // 
-            this.textBox3.Location = new System.Drawing.Point(94, 164);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(133, 25);
-            this.textBox3.TabIndex = 6;
+            this.button3.Location = new System.Drawing.Point(681, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "threshold";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.threshold_Click);
             // 
-            // textBox2
+            // button4
             // 
-            this.textBox2.Location = new System.Drawing.Point(94, 115);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(133, 25);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(94, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(133, 25);
-            this.textBox1.TabIndex = 4;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 125);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 15);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "성별";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 88);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "환자이름";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 55);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "챠트번호";
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(255, 33);
-            this.panel2.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "환자정보";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(14, 27);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 15);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Preview";
-            // 
-            // picSnapShot
-            // 
-            this.picSnapShot.Location = new System.Drawing.Point(338, 47);
-            this.picSnapShot.Name = "picSnapShot";
-            this.picSnapShot.Size = new System.Drawing.Size(334, 270);
-            this.picSnapShot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picSnapShot.TabIndex = 4;
-            this.picSnapShot.TabStop = false;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(335, 29);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(72, 15);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "SnapShot";
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Location = new System.Drawing.Point(17, 323);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(112, 39);
-            this.btnPlay.TabIndex = 6;
-            this.btnPlay.Tag = "0";
-            this.btnPlay.Text = "Play";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
-            // 
-            // btnSnapShot
-            // 
-            this.btnSnapShot.Location = new System.Drawing.Point(200, 323);
-            this.btnSnapShot.Name = "btnSnapShot";
-            this.btnSnapShot.Size = new System.Drawing.Size(112, 39);
-            this.btnSnapShot.TabIndex = 7;
-            this.btnSnapShot.Text = "SnapShot";
-            this.btnSnapShot.UseVisualStyleBackColor = true;
-            this.btnSnapShot.Click += new System.EventHandler(this.btnSnapShot_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.button4.Location = new System.Drawing.Point(214, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "changeall";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.changeall_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(951, 553);
-            this.Controls.Add(this.btnSnapShot);
-            this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.picSnapShot);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.picPreview);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CB);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picSnapShot)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox picPreview;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CB;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.PictureBox picSnapShot;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button btnPlay;
-        private System.Windows.Forms.Button btnSnapShot;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,58 +1,204 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Size = OpenCvSharp.Size;
 
-using OpenCvSharp;
-
-namespace TestApp
+namespace imageblack
 {
     public partial class Form1 : Form
     {
-
-        OpenCvSharp.VideoCapture cap = new VideoCapture(@"c:\Temp\Video.mp4");
-        Mat frame = new Mat();
-
-
+        Image image;
+        Bitmap gBitmap;
+        string image_path = null;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnSnapShot_Click(object sender, EventArgs e)
+        private void search_Click(object sender, EventArgs e)//파일선택
         {
-            picSnapShot.Image = picPreview.Image; 
-        }
-
-        private void btnPlay_Click(object sender, EventArgs e)
-        {
-            if (btnPlay.Text == "Play")//Stop
+            try
             {
-                btnPlay.Text = "Stop";
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    image_path = ofd.FileName;
+                    pictureBox1.Image = Image.FromFile(image_path);
+
+                    image = pictureBox1.Image;
+                    gBitmap = new Bitmap(image);
+                }
             }
-            else
+            catch (Exception ex1)
             {
-                btnPlay.Text = "Play";
+                MessageBox.Show(ex1.Message);
             }
-
-            timer1.Enabled = !timer1.Enabled;
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void changeall_Click(object sender, EventArgs e)
         {
-           cap.Read(frame);
+            FolderBrowserDialog fbd1 = new FolderBrowserDialog();//jpg, txt 폴더 위치
+            FolderBrowserDialog fbd2 = new FolderBrowserDialog();//저장될 폴더 위치
 
-           picPreview.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
+            if(fbd1.ShowDialog() == DialogResult.OK)
+            {
+                if (fbd2.ShowDialog() == DialogResult.OK)
+                {
+                    string[] imgfiles = Directory.GetFiles(fbd1.SelectedPath, "*.jpg");//jpg파일
+                    string[] txtfiles = Directory.GetFiles(fbd1.SelectedPath, "*.txt");//txt파일
+
+                    int n = 0;//n번째 파일
+
+                    while (n < imgfiles.GetLength(0))
+                    {
+                        image = Image.FromFile(imgfiles[n]);
+                        gBitmap = new Bitmap(image);
+
+
+
+                        //흑백
+                        /*int brightness;
+                        Color color, gray;
+
+                        for (int x = 0; x < image.Width; x++)
+                        {
+                            for (int y = 0; y < image.Height; y++)
+                            {
+                                color = gBitmap.GetPixel(x, y);
+                                brightness = (int)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B);
+                                gray = Color.FromArgb(brightness, brightness, brightness);
+                                gBitmap.SetPixel(x, y, gray);
+                            }
+                        }*/
+
+
+
+                        //가우시안
+                        /*Mat src = Cv2.ImRead(imgfiles[n]);
+                        Mat dst = new Mat();
+
+                        //Cv2.BilateralFilter(src, dst, 5, 250, 10);
+                        Cv2.GaussianBlur(src, dst, new Size(5, 5), 3, 3); //가우시안
+                        //Cv2.MedianBlur(src, dst, 3);
+
+                        gBitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);//dst[] 0.bilateral  1.gaussian  2.median*/
+
+
+
+                        //threshold
+                        Mat tmp = new Mat(imgfiles[n], ImreadModes.AnyColor);//유색
+                        Mat src = new Mat(imgfiles[n], ImreadModes.Grayscale);//흑백
+
+                        Mat dst = new Mat();
+
+                        Cv2.Threshold(src, dst, 140, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);//127: 가중치 조절가능
+
+                        gBitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+
+                        //가우시안 + threshold
+                        /*Mat src = Cv2.ImRead(imgfiles[n]);
+                        Mat dst = new Mat();
+
+                        //Cv2.BilateralFilter(src, dst, 5, 250, 10);
+                        Cv2.GaussianBlur(src, dst, new Size(5, 5), 3, 3); //가우시안
+                        //Cv2.MedianBlur(src, dst, 3);
+
+                        Mat dst2 = new Mat();
+
+                        Cv2.Threshold(dst, dst2, 140, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);//127: 가중치 조절가능
+
+                        gBitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst2);*/
+
+                        string[] pathsplit = imgfiles[n].Split('\\');
+                        string name = pathsplit[pathsplit.GetLength(0) - 1];//jpgname
+
+                        string[] txtpathsplit = txtfiles[n++].Split('\\');
+                        string txtname = txtpathsplit[txtpathsplit.GetLength(0) - 1];//txtname
+
+                        image = gBitmap;
+                        image.Save(fbd2.SelectedPath + "\\gausthres" + name);//저장폴더경로 + 파일 이름
+                        File.Copy(fbd1.SelectedPath + "\\" + txtname, fbd2.SelectedPath + "\\gausthres" + txtname);//텍스트파일 복붙
+                    }
+                }
+            }
         }
 
-        private void timer2_Tick(object sender, EventArgs e)
+        private void CB_Click(object sender, EventArgs e)//흑백
         {
+            int brightness;
+            Color color, gray;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    color = gBitmap.GetPixel(x, y);
+                    brightness = (int)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B);
+                    gray = Color.FromArgb(brightness, brightness, brightness);
+                    gBitmap.SetPixel(x, y, gray);
+                    pictureBox2.Image = gBitmap;
+                }
+            }
+            /*Color co;
+            int Average; //RGB값의 평균
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    co = gBitmap.GetPixel(x, y);
+                    Average = (co.R + co.G + co.B) / 3;
+
+                    // R.G.B 를 모두 같은 값으로 지정하면 회색임.
+                    co = Color.FromArgb(Average, Average, Average);
+
+                    gBitmap.SetPixel(x, y, co);     // 해당 좌표 픽셀의 컬러값을 변경
+                }
+            }*/
+        }
+
+        private void gaussian_Click(object sender, EventArgs e)//가우시안
+        {
+            Mat src = Cv2.ImRead(image_path); 
+            Mat[] dst = new Mat[3] { new Mat(), new Mat(), new Mat() }; 
+
+            Cv2.BilateralFilter(src, dst[0], 5, 250, 10); 
+            Cv2.GaussianBlur(src, dst[1], new Size(5, 5), 3, 3); //가우시안
+            Cv2.MedianBlur(src, dst[2], 3);
+
+            gBitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst[1]);//dst[] 1.bilateral  2.gaussian  3.median
+            pictureBox2.Image = gBitmap;
+
+            /*Cv2.ImShow("src", src); 
+            Cv2.ImShow("Bilateral", dst[0]); 
+            Cv2.ImShow("Gaussian", dst[1]); 
+            Cv2.ImShow("Median", dst[2]); 
+
+            Cv2.WaitKey(0); 
+            Cv2.DestroyAllWindows();*/
+
+        }
+
+        private void threshold_Click(object sender, EventArgs e)
+        {
+            Mat tmp = new Mat(image_path, ImreadModes.AnyColor);//유색
+            Mat src = new Mat(image_path, ImreadModes.Grayscale);//흑백
+
+            Mat dst = new Mat();
+
+            Cv2.Threshold(tmp, dst, 127, 255, ThresholdTypes.Binary);//127: 가중치 조절가능
+
+            gBitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(dst);
+            pictureBox2.Image = gBitmap;
         }
     }
 }
